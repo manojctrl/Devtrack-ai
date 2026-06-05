4e221bb334cebe5304f7d9769e5cd2a5a8badbb7
Manoj Katuwal <mkc684867@gmail.com>
2026-06-05 16:48:09 +0545

    setup for dashboard interface

---
+++ frontend/src/Pages/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../components/dashboard/sidebar"
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Sidebar />
+
+    </div>
+  )
+}
+
+export default Dashboard
--- frontend/src/router/AppRouter/AppRouter.jsx
+++ frontend/src/router/AppRouter/AppRouter.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Landing from "../../Pages/Landing/Landing"
 import { Login } from "../../Pages/Login/Login"
+import Dashboard from "../../Pages/dashboard/Dashboard"
 
 const AppRouter = () => {
   return (
     <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Login />}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
         
     </Routes>
   )
