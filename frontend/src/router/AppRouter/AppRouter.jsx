03202bf91abc7f4ad342ba38576b500973a82387
Manoj Katuwal <mkc684867@gmail.com>
2026-06-05 09:46:09 +0545

    finish login page and just started regster page

--- frontend/src/router/AppRouter/AppRouter.jsx
+++ frontend/src/router/AppRouter/AppRouter.jsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Landing from "../../Pages/Landing/Landing"
 import { Login } from "../../Pages/Login/Login"
-import Register from "../../Pages/Register/Register"
 
 const AppRouter = () => {
   return (
     <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register/>}/>
         
     </Routes>
   )
