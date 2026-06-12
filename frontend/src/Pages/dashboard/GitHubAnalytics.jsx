1372a22fbfc39b08a65bbb5ce87259249ccfd69a
Manoj Katuwal <mkc684867@gmail.com>
2026-06-12 19:24:58 +0545

    Github Aanalytics page has been addes

--- frontend/src/Pages/dashboard/GitHubAnalytics.jsx
+++ frontend/src/Pages/dashboard/GitHubAnalytics.jsx
@@ -1,16 +1,15 @@
-import Navbar from "../../components/dashboard/Navbar"
-import Sidebar from "../../components/dashboard/sidebar"
+import Navbar from "../../components/dashboard/Navbar";
+import Sidebar from "../../components/dashboard/sidebar";
 
 const GitHubAnalytics = () => {
   return (
-        <div className=" min-h-screen bg-[#111625] flex text-gray-100 font-sans">
-            <Sidebar />
-                  <div className="flex-1 pl-64">
-                    <Navbar />
-</div>
-        </div>
+    <div className=" min-h-screen bg-[#111625] flex text-gray-100 font-sans">
+      <Sidebar />
+      <div className="flex-1 pl-64">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default GitHubAnalytics
+export default GitHubAnalytics;
