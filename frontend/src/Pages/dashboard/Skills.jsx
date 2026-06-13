3100821763dd2c25550571819b133283b002a55b
Manoj Katuwal <mkc684867@gmail.com>
2026-06-13 11:23:15 +0545

    skills page just began

---
+++ frontend/src/Pages/dashboard/Skills.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../../components/dashboard/Navbar";
+import Sidebar from "../../components/dashboard/sidebar";
+
+const Skills = () => {
+  return (
+    <div className="min-h-screen bg-[#111625] flex text-gray-100 font-sans">
+      <Sidebar />
+      <div className="flex-1 pl-64">
+        <Navbar />
+        <main className="p-8 pt-24 max-w-7xl mx-auto space-y-8"></main>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
--- frontend/src/router/AppRouter/AppRouter.jsx
+++ frontend/src/router/AppRouter/AppRouter.jsx
@@ -4,6 +4,7 @@ import { Login } from "../../Pages/Login/Login"
 import Dashboard from "../../Pages/dashboard/Dashboard"
 import Profile from "../../Pages/dashboard/Profile"
 import GitHubAnalytics from "../../Pages/dashboard/GitHubAnalytics"
+import Skills from "../../Pages/dashboard/Skills"
 
 const AppRouter = () => {
   return (
@@ -13,6 +14,7 @@ const AppRouter = () => {
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route  path="/profile" element={<Profile />}/>
         <Route path="/github-analytics" element={<GitHubAnalytics />}/> 
+        <Route path="/skills" element={<Skills />} />
         
     </Routes>
   )
