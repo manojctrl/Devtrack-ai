43ec89aac6d4f20b0c991f8b5e26a5060dd621d1
Manoj Katuwal <mkc684867@gmail.com>
2026-06-14 11:21:46 +0545

    user table has been created using sequelize

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+
+  githubUsername: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const User = require("./models/User");
 
 const app = require("./app");
 const sequelize = require("./config/database");
@@ -7,8 +8,12 @@ const PORT = process.env.PORT || 5000;
 
 sequelize
   .authenticate()
-  .then(() => {
+  .then(async () => {
     console.log("✅ Database Connected");
+
+    await sequelize.sync();
+
+    console.log("✅ Tables Synced");
 
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
