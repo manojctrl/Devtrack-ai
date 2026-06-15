e3c1416952860d120c70e01c674613e043cb4e21
Manoj Katuwal <mkc684867@gmail.com>
2026-06-15 10:39:22 +0545

    auth middleware has been made

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes")
+const userRoutes = require("./routes/userRoutes")
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes)
 
 app.get("/", (req, res) => {
   res.send("DevTrack AI Backend Running 🚀");
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = express.Router()
+
+
+
+const authMiddleware = require("../middleware/authMiddleware")
+
+
+router.get('/profile', authMiddleware , (req, res)=> {
+    res.json({
+        message: "Protected route Accessed", 
+        user : req.user
+    })
+})
+
+module.exports = router
